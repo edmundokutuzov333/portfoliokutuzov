1e15a87caf80bb12506026ea47c18943186de4c2
edmundokutuzov333 <contact@edmundokutuzov.art>
2026-09-24 18:07:16 +0200

    chore(routes): remove duplicate portuguese portfolio lazy route

--- src/routes/pt.portfolio.index.lazy.tsx
+++
@@ -1,6 +0,0 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
-import { PortfolioRoutePage } from "@/components/portfolio/PortfolioRoutePage";
-
-export const Route = createLazyFileRoute("/pt/portfolio")({
-  component: PortfolioRoutePage,
-});
