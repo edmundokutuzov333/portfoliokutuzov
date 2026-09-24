46ca037401e93d5d0a548f60c9051d0ad83cd3a9
edmundokutuzov333 <contact@edmundokutuzov.art>
2026-09-24 17:36:18 +0200

    fix(HON-04): remove inline eslint disable

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-refresh/only-export-components */
 import { createRouter, useRouter } from "@tanstack/react-router";
 import { QueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
